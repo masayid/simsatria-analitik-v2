f89ba56772c590dff9856d766c0831255e819a68
masayid <masayid09@gmail.com>
2026-08-13 17:19:01 +0700

    chore: add permission scaffold

---
+++ 01_CORE/Permission.js
@@ -0,0 +1,9 @@
+/** Permission matrix - scaffold. */
+const SATRIA_PERMISSIONS = {
+  VIEW: 'VIEW',
+  CREATE: 'CREATE',
+  UPDATE: 'UPDATE',
+  DELETE: 'DELETE',
+  EXPORT: 'EXPORT',
+  ADMIN: 'ADMIN'
+};
