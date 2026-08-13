46a63430e97a95a2e033b7a64bf42de0d3fa97b0
masayid <masayid09@gmail.com>
2026-08-13 17:19:09 +0700

    chore: add MASTER_SEKOLAH schema

---
+++ 02_MASTER/MASTER_SEKOLAH.js
@@ -0,0 +1,5 @@
+/** MASTER_SEKOLAH: registry sekolah dalam instalasi multi-sekolah. */
+const MASTER_SEKOLAH = {
+  SHEET: 'MASTER_SEKOLAH',
+  HEADERS: ['idSekolah','npsn','namaSekolah','alamat','kabupaten','provinsi','status','spreadsheetId','aktif']
+};
