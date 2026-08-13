e6c1c4ff02cbb53535d58518db2495a387e55e05
masayid <masayid09@gmail.com>
2026-08-13 17:19:58 +0700

    feat: initialize SIM SATRIA web app entry point

--- Code.js
+++ Code.js
@@ -1,3 +1,13 @@
-function myFunction() {
-  
+/**
+ * SIM SATRIA Multi Sekolah - entry point.
+ */
+function doGet() {
+  return HtmlService.createTemplateFromFile('05_UI/index')
+    .evaluate()
+    .setTitle(SATRIA_CONFIG.APP_NAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
